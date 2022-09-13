b0abe834d3fb286fd7d9651440c51776e369cca5
pablo <pablo.mandile@gmail.com>
2022-09-13 14:11:20 -0300

    cambios en los estilos, fuente digital y fondo

---
+++ js/calculadora.js
@@ -0,0 +1,13 @@
+let numero1 = 2;
+let numero2 = "5";
+
+//alert("Hola usuario");
+
+// let edad = prompt("Ingrese su edad");
+// console.log(edad);
+// let mensaje = (edad >= 18) ? "Bienvenido al sitio" : "Este sitio está restringido";
+// console.log(mensaje);
+
+// numero2 = numero1 + parseInt(numero2);
+numero2 += numero1;
+console.log(numero2);
